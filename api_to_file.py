8e91fdc9a722a52d3a37cf34523d42797e21a87b
Flabby Ninja <dev@flabbyninja.net>
2018-06-12 08:46:16 +0100

    Add support for local directory location

--- api_to_file.py
+++ api_to_file.py
@@ -1,25 +1,28 @@
 import pickle
 import json
 import sheets_api
+from os.path import join
 
 
-def save_to_file(data, filename, format='json'):
+def save_to_file(data, filename, local_dir='.', format='json'):
+    full_filename = join(local_dir, filename)
     if format == 'pickle':
-        afile = open(filename, 'wb')
+        afile = open(full_filename, 'wb')
         pickle.dump(data, afile)
     else:
-        afile = open(filename, 'w')
+        afile = open(full_filename, 'w')
         js = json.dumps(data)
         afile.write(js)
     afile.close()
 
 
-def load_from_file(filename, format='json'):
+def load_from_file(filename, local_dir='.', format='json'):
+    full_filename = join(local_dir, filename)
     if format == 'pickle':
-        source_file = open(filename, 'rb')
+        source_file = open(full_filename, 'rb')
         new_data = pickle.load(source_file)
     else:
-        source_file = open(filename, 'r')
+        source_file = open(full_filename, 'r')
         js = source_file.read()
         new_data = json.loads(js)
     source_file.close()
@@ -33,5 +36,5 @@ if __name__ == '__main__':
     print('Retrieving data')
     values = sheets_api.load_sheet_data(service, sheet_id, 'Current!A2:AV50')
     print('Persisting data')
-    save_to_file(values, 'sheet.json')
+    save_to_file(values, 'sheet.json', 'temp')
     print('Done')
