f24431a55aad1143636411e72c568948c88681be
Flabby Ninja <dev@flabbyninja.net>
2018-06-12 08:39:41 +0100

    Add filename parameter and switch to JSON format

--- api_to_file.py
+++ api_to_file.py
@@ -1,17 +1,28 @@
 import pickle
+import json
 import sheets_api
 
 
-def save_to_file(data):
-    afile = open(r'sheet.pkl', 'wb')
-    pickle.dump(data, afile)
+def save_to_file(data, filename, format='json'):
+    if format == 'pickle':
+        afile = open(filename, 'wb')
+        pickle.dump(data, afile)
+    else:
+        afile = open(filename, 'w')
+        js = json.dumps(data)
+        afile.write(js)
     afile.close()
 
 
-def load_from_file():
-    file2 = open(r'sheet.pkl', 'rb')
-    new_data = pickle.load(file2)
-    file2.close()
+def load_from_file(filename, format='json'):
+    if format == 'pickle':
+        source_file = open(filename, 'rb')
+        new_data = pickle.load(source_file)
+    else:
+        source_file = open(filename, 'r')
+        js = source_file.read()
+        new_data = json.loads(js)
+    source_file.close()
     return new_data
 
 
@@ -22,5 +33,5 @@ if __name__ == '__main__':
     print('Retrieving data')
     values = sheets_api.load_sheet_data(service, sheet_id, 'Current!A2:AV50')
     print('Persisting data')
-    save_to_file(values)
+    save_to_file(values, 'sheet.json')
     print('Done')
