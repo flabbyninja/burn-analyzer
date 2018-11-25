0526909b345eb7e0685ca8dbcfcb803f34ea3010
Flabby Ninja <dev@flabbyninja.net>
2018-11-25 08:57:28 -0800

    Split out ingestion processing

---
+++ ingestion/burn_analyzer/sheets_api.py
@@ -0,0 +1,28 @@
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import service_account
+
+from burn_analyzer import config, api_to_file
+
+
+def initialise_sheets_api(creds_file, scopes):
+    creds = service_account.ServiceAccountCredentials.from_json_keyfile_name(creds_file, scopes)
+    return build('sheets', 'v4', http=creds.authorize(Http()), cache_discovery=False)
+
+
+def load_sheet_data(service, spreadsheet_id, range_name):
+    result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name, majorDimension='COLUMNS').execute()
+    values = result.get('values', [])
+    return values
+
+
+if __name__ ==  "__main__":
+    creds_file, scope, sheet_id, local_mode, local_dir, _ = config.load_settings()
+
+    if local_mode:
+        print('Running in local mode')
+        exercise_data = api_to_file.load_from_file('sheets.json', local_dir)
+    else:
+        service = initialise_sheets_api(creds_file, scope)
+        exercise_data = load_sheet_data(service, sheet_id, 'Current!A2:AV50')
+    print(exercise_data)
