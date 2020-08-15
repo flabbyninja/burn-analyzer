61b759e6a69c5cb6636f002fccf7105076c6429e
Flabby Ninja <dev@flabbyninja.net>
2020-08-15 22:28:31 +0100

    Add sample descriptions. Tweak responsive layout.

--- src/Cards.js
+++ src/Cards.js
@@ -8,7 +8,7 @@ function Cards(props) {
     <div className="carddeck row">
       {cardConfig.map((value, index) => {
         return (
-          <div className="card-holder p-2 col-xl-2 col-md-3 col-sm-6">
+          <div className="card-holder d-flex align-items-stretch p-2 col-lg-2 col-md-3 col-sm-4 col-6">
             <Card card={value} />
           </div>
         );
@@ -24,7 +24,7 @@ function Card(props) {
       <div class="card-body">
         <h5 class="card-title">{props.card.name}</h5>
         <p class="card-text">{props.card.description}</p>
-        <a href={props.card.dataSource} class="btn btn-primary">
+        <a href={props.card.dataSource} className="btn btn-primary btn-sm">
           API Link
         </a>
       </div>
--- src/testdata.js
+++ src/testdata.js
@@ -1,49 +1,52 @@
 const staticCards = [
   {
     name: "The Ripper",
-    description: "decription",
+    description: "Pain like you've never felt before, but in your t*ts",
     dataSource: "api/thing",
     icon: "flaticon-032-fitness",
     priority: 1,
   },
   {
     name: "Arm Curl",
-    description: "decription",
+    description: "Try to curl like Arnie, but you'll cry like a small child",
     dataSource: "api/thing",
     icon: "flaticon-019-sportman",
     priority: 2,
   },
   {
     name: "Leg Press",
-    description: "decription",
+    description:
+      "Push, you weed. No-one wants muscles up top and matchsticks for legs!",
     dataSource: "api/thing",
     icon: "flaticon-018-shorts",
     priority: 3,
   },
   {
     name: "Chest Press",
-    description: "decription",
+    description:
+      "Sounds easy. It'd be easier if you'd practiced push ups, but you never did...",
     dataSource: "api/thing",
     icon: "flaticon-031-six-pack",
     priority: 4,
   },
   {
     name: "Leg Raise",
-    description: "decription",
+    description: "Deceptively simple. Deliciously hard.",
     dataSource: "api/thing",
     icon: "flaticon-018-shorts",
     priority: 5,
   },
   {
     name: "Lateral Pull",
-    description: "decription",
+    description: "Exerices a small, but important muscle. No, not that one...",
     dataSource: "api/thing",
     icon: "flaticon-049-muscle",
     priority: 6,
   },
   {
     name: "Shoulder Press",
-    description: "decription",
+    description:
+      "Build your neck so big no-one knows you have one, you bullet-heid you...",
     dataSource: "api/thing",
     icon: "flaticon-017-tank-top",
     priority: 7,
